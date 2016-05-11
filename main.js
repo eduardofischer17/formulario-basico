ef9de56ca0a7cf0c0bb0fb7baa3b39a6aac519b9
Eduardo Fischer <Eduardo Fischer>
2016-05-10 23:52:50 -0400

    primer formulario

---
+++ main.js
@@ -0,0 +1,43 @@
+(function () {
+// Variables
+
+	var formulario = document.getElementById("formulario");
+	var elementos = formulario.elements;
+	var boton = document.getElementById("btn");
+
+// Funciones
+	var validarNombre = function (e) {
+		if (formulario.nombre.value == 0) {
+			alert("El campo nombre esta vacio");
+			e.preventDefault();
+		} 
+	};
+
+	var validarSexo = function (e) {
+		if (formulario.Radio1.checked == true || formulario.Radio2.checked == true ) {
+		} else {
+			alert("Debe seleccionar el sexo");
+			e.preventDefault();
+		}
+	};
+
+	var validarTerminos = function (e) {
+		if (formulario.Terminoscondiciones.checked == false) {
+			alert("Debe aceptar los terminos y condiciones");
+			e.preventDefault();
+		} 
+	};
+
+	var validar = function (e) {
+		validarNombre(e);
+		validarSexo(e);
+		validarTerminos(e);
+};
+
+// Eventoss
+	formulario.addEventListener("submit", validar);
+
+var Todo = varlidarNombre;
+	document.write(Todo);
+
+}())
