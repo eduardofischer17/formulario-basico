625f479a6b17de3c55effb921eacf43fe08292e2
Victor Rivas <vctr90w@gmail.com>
2016-05-14 08:54:35 -0500

    Show input text at the end of the document

--- main.js
+++ main.js
@@ -6,34 +6,40 @@
 	var boton = document.getElementById("btn");
  
 // Funciones
-	var validarNombre = function (configuration) {
-            var nombreElement = new configuration.domElementToValidate(formulario.nombre);
-            var event = configuration.event;
+	var validarNombre = function (domElementToValidate) {
+            var nombreElement = new domElementToValidate(formulario.nombre);
             var nombreElementVal = nombreElement.getElementValue();
+            var showNameElementSelector = '#showName';
+            var $showName = $(showNameElementSelector);
 		if (nombreElementVal === '') {
 			alert("El campo nombre esta vacio");
-			event.preventDefault();
-		}
+		} else {
+                  $showName.html('Name: ' + nombreElementVal);
+                }
 	}
 
-	var validarSexo = function (configuration) {
-            var sexoElement1 = new configuration.domElementToValidate(formulario.Radio1);
-            var sexoElement2 = new configuration.domElementToValidate(formulario.Radio2);
-            var event = configuration.event;
-		if (sexoElement1.getElementCheckedProperty() == true || sexoElement2.getElementCheckedProperty() == true ) {
+	var validarSexo = function (domElementToValidate) {
+            var sexoElement1 = new domElementToValidate(formulario.Radio1);
+            var sexoElement2 = new domElementToValidate(formulario.Radio2);
+            var showSexElementSelector = '#showSex';
+            var $showSex = $(showSexElementSelector);
+
+		if (sexoElement1.getElementCheckedProperty() === true || sexoElement2.getElementCheckedProperty() === true ) {
+                  $showSex.html('Sex: ' + sexoElement1.getElementCheckedProperty() ? sexoElement1.getElementValue() : sexoElement2.getElementValue());
 		} else {
 			alert("Debe seleccionar el sexo");
-			event.preventDefault();
 		}
 	};
 
-	var validarTerminos = function (configuration) {
-          var terminosYCondicionesElement = new configuration.domElementToValidate(formulario.Terminoscondiciones);
-          var event = configuration.event;
-		if (terminosYCondicionesElement.getElementCheckedProperty() == false) {
+	var validarTerminos = function (domElementToValidate) {
+          var terminosYCondicionesElement = new domElementToValidate(formulario.Terminoscondiciones);
+          var showTermsAndConditionsElementSelector = '#showTermsAndConditions';
+          var $showTermsAndConditions = $(showTermsAndConditionsElementSelector);
+		if (terminosYCondicionesElement.getElementCheckedProperty() === false) {
 			alert("Debe aceptar los terminos y condiciones");
-			event.preventDefault();
-		} 
+		} else {
+                  $showTermsAndConditions.html('Terms and conditions accepted');
+                }
 	};
 
 	var validar = function (e) {
@@ -51,14 +57,12 @@
             return this.elementToValidate.checked;
           };
 
-          var configurationForValidation = {
-            domElementToValidate: domElementToValidate,
-            event: e,
-          };
-
-          validarNombre(configurationForValidation);
-          validarSexo(configurationForValidation);
-          validarTerminos(configurationForValidation);
+          validarNombre(domElementToValidate);
+          validarSexo(domElementToValidate);
+          validarTerminos(domElementToValidate);
+          // This event.preventDefault() prevents the browser from doing submit default action
+          // which we have fired to start the validation
+          e.preventDefault();
         };
 
 // Eventoss
